0dd1a246ca606399b9aa63b341f4fc81b209f274
Andrew Jones and Charley McGowan <andrew@>
2018-10-03 19:36:18 -0700

    Got it to do everything but the multiple consonants and figuring out how to handle multiple vowels in a single word

--- js/scripts.js
+++ js/scripts.js
@@ -4,65 +4,79 @@ var pigLatin = function(input) {
   console.log("Hey this is the input: " + input);
   var sentenceArray = input.split(" ")
   var outputArray = [];
-  sentenceArray.forEach(function(x) {
-
-    console.log("");
-    console.log("----------------");
-    console.log("ForEach value of X: " + x)
-
-    vowels.forEach(function(y){
-      lowestVowel = 0;
-      if ( x.indexOf(y) > 0) {
-
-        //if (lowestVowel == 0) {
-
-          lowestVowel += 1;
-          console.log("forEach value of Y: " + y);
-
-          var intermediate = [];
-          intermediate = x.split('');
-          var firstConsonants = [];
-
-          console.log("this is the value of x.indexOf(y): " + x.indexOf(y));
-
-          for (var i = 0; i < x.indexOf(y); i++) {
-
-            console.log("Here's the value of i: " + i);
-            firstConsonants.push(intermediate[i]);
-            console.log("This is firstConsonants: " + firstConsonants);
+  var vowelCountPlaceholder = 0;
 
 
-            };
-          console.log("This is the value of 'intermediate' array: " + intermediate);
-          intermediate.push(intermediate.splice(0, 1)[0]);
-          intermediate.push("ay")
-          console.log("This is the value of 'intermediate' array after the switch: " + intermediate)
-          console.log("This is the value of 'intermediate' array after the switch, and joined: " + intermediate.join(""))
+    console.log("This is where the if loop starts----");
+    sentenceArray.forEach(function(x) {
 
-        } //else {
-            //lowestVowel == 0;
-            //}
-        /*
-      console.log("");
-      console.log("this is the vowel I'm checking: " + y)
-      console.log("This is the index of a vowel for this iteration: " + x.indexOf(y));
-      console.log("");*/
+      // console.log("");
+      // console.log("---------------------------------------------------------------");
+      // console.log("ForEach value of X: " + x)
+
+      vowels.forEach(function(y){
+          if (vowelCountPlaceholder < 1) {
+        // console.log("");
+        // console.log("this is the vowel I'm checking: " + y)
+        // console.log("This is the index of a vowel for this iteration: " + x.indexOf(y));
+        // console.log("");
+
+            if ( x.indexOf(y) > 0) {
+                // console.log("forEach value of Y: " + y);
+
+                var intermediate = [];
+                intermediate = x.split('');
+
+                // console.log("this is the value of x.indexOf(y): " + x.indexOf(y));
+
+                for (var i = 0; i < x.indexOf(y); i++) {
+                  // console.log("Here's the value of i: " + i);
+                  };
+
+                // console.log("This is the value of 'intermediate' array: " + intermediate);
+
+                intermediate.push(intermediate.splice(0, 1)[0]);
+                intermediate.push("ay")
+
+                // console.log("This is the value of 'intermediate' array after the switch: " + intermediate)
+                // console.log("This is the value of 'intermediate' array after the switch, and joined: " + intermediate.join(""))
+                outputArray.push(intermediate.join(""));
+
+              } else if (x.indexOf(y) === 0){
+                // console.log("This is a word starting with a vowel.");
+                var intermediate = [];
+                intermediate.push(x);
+                intermediate.push("way");
+                // console.log("This is intermediate as an array: " + intermediate);
+
+                // console.log("this is the type of intermediate after the join: " + typeof intermediate);
+                // console.log("This is intermediate after the push and join: " + intermediate.join(""));
+                outputArray.push (intermediate.join(""));
+
+              }
+          console.log("This is vowelCountPlaceholder: " + vowelCountPlaceholder);
+          vowelCountPlaceholder++;
+          console.log("This is where the IF loop stops ---------- ");
+        };
+        vowelCountPlaceholder = 0;
+        ;console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        console.log("");
+      //var firstVowel = x.indexOf("a")
       });
-      ;console.log("^^^^^^^^^^^^^^");
-      console.log("");
-    //var firstVowel = x.indexOf("a")
-    });
-  return sentenceArray;
+      console.log("This is outputArray: " + outputArray.join(" "));
+
+    return outputArray.join(" ");
+  });
 };
 
 //jquery user interface
 $(document).ready(function(){
-  $("form#pig-latin").submit(function(){
-    event.preventDefault();
-    var pigLatinInput = $("input#pig-latin").val();
-    var result = pigLatin(pigLatinInput);
+$("form#pig-latin").submit(function(){
+  event.preventDefault();
+  var pigLatinInput = $("input#pig-latin").val();
+  var result = pigLatin(pigLatinInput);
 
-    $("#output").text(result);
-    $("#result").show(result);
+  $("#output").text(result);
+  $("#result").show(result);
   });
 });
