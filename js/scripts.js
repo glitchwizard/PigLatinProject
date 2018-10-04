5cefa02830017fbcd2a6432a1132124c385bebd8
Andrew Jones and Charley McGowan <andrew@>
2018-10-03 21:04:22 -0700

    wasnt fully working, trying to sort out the double letters like for a word like poach

--- js/scripts.js
+++ js/scripts.js
@@ -4,34 +4,48 @@ var pigLatin = function(input) {
   var sentenceArray = input.split(" ")
   var outputArray = [];
   var vowelCountPlaceholder = 0;
-
     sentenceArray.forEach(function(x) {
-
       vowels.forEach(function(y){
-            if ( x.indexOf(y) > 0) {
+        if ( x.indexOf(y) > 0 && /[aeiouAEIOU]/.test(x) === true ){
+          if (vowelCountPlaceholder < 1) {
+            var intermediate = [];
+            intermediate = x.split('');
+            intermediate.forEach(function(z) {
+              console.log("here's the value of z: " + z);
+              vowels.forEach(function(zz){
+                console.log("here's the value of zz it's checking against: " + zz );
                 if (vowelCountPlaceholder < 1) {
-                  var intermediate = [];
-                  intermediate = x.split('');
-                  for(var j = 0; j < x.indexOf(y); j++){
+                  if (zz === z && /qu/.test(x) === false) {
+                    for(var j = 1; j < x.indexOf(y); j++){
+                      console.log("If this is true, splice! " + intermediate);
+                      intermediate.push(intermediate.splice(0, 1)[0]);
+                    }
+                  } else if (zz === z && /qu/.test(x) === true){
+                    for(var j = 1; j < x.indexOf(y)+1; j++){
+                    console.log("If this is true, splice! " + intermediate);
                     intermediate.push(intermediate.splice(0, 1)[0]);
+                    }
+                    vowelCountPlaceholder++;
                   }
-                  intermediate.push("ay")
-                  outputArray.push(intermediate.join(""));
-                  vowelCountPlaceholder++;
                 }
-            } else if (x.indexOf(y) === 0) {
-                  if (vowelCountPlaceholder < 1) {
-                  var intermediate = [];
-                  intermediate.push(x);
-                  intermediate.push("way");
-                  outputArray.push (intermediate.join(""));
-                  vowelCountPlaceholder++;
-                }
-              }
+              });
+            });
+            intermediate.push("ay")
+            outputArray.push(intermediate.join(""));
+            vowelCountPlaceholder++;
+          }
+        } else if (x.indexOf(y) === 0 && /qu/.test(x) === false) {
+          if (vowelCountPlaceholder < 1) {
+            var intermediate = [];
+            intermediate.push(x);
+            intermediate.push("way");
+            outputArray.push (intermediate.join(""));
+            vowelCountPlaceholder++;
+          }
+        }
       });
       vowelCountPlaceholder =0;
     });
-    console.log("This is outputArray: " + outputArray.join(" "));
     return outputArray.join(" ");
   };
 
